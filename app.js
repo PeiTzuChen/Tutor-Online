c0fb93bdac8c9b684fe4acac0b19d5e47afbe1b3
peitzuchen <prettysna@hotmail.com>
2024-02-19 17:17:24 +0800

    fix bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if ((process.env.NODE_ENV === 'development')) require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const route = require('./routes')
 const session = require('express-session')
 const MemoryStore = require('memorystore')(session)
