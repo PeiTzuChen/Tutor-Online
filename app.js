e1298715ff3cb555e55528c0a823483b76c9236d
peitzuchen <prettysna@hotmail.com>
2024-03-05 23:35:44 +0800

    test cors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const passport = require('./config/passport')
 const server = http.Server(app)
 const { Server } = require('socket.io')
 
+const { createClient } = require('redis')
+
 // cross-origin
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.103:3000')
@@ -16,7 +18,7 @@ app.use((req, res, next) => {
     'GET, POST, PUT, DELETE, PATCH')
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Content-Type, authorization, accept, accept-encoding'
+    'Content-Type, Authorization, Accept, Accept-Encoding'
   )
   next()
 })
@@ -26,9 +28,32 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
+const redis = async (id, data) => {
+  const client = createClient({ url: 'redis://127.0.0.1:6379' })
+  client.on('ready', () => {
+    console.log('Redis is ready')
+  })
+  client.on('error', (err) => {
+    console.log("Redis' error", err)
+  })
+  await client.connect()
+  const list = {}
+  list[id] = data
+
+  await client.lPush('dialog', JSON.stringify(list))
+
+  await client.quit()
+}
+
 const io = new Server(server)
+const list = {}
 io.on('connection', (socket) => {
-  console.log('userID: ', socket.id)
+  const userId = socket.id
+  list[userId] = 'test' // F_PV4fqZgWv03jUgAAAC: 'test'  eachId has own avatar
+  socket.on('message', (id, data) => {
+    redis(id, data)
+    socket.broadcast.emit('message', list[id], data)
+  })
 })
 
 app.use(express.json())
