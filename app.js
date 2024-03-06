d958d48f06a49861b57230f147b0cb3a9d6ad488
peitzuchen <prettysna@hotmail.com>
2024-03-06 16:30:06 +0800

    test cors11

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ const { createClient } = require('redis')
 // http://192.168.1.103:3000
 // cross-origin
 app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') res.status(200)
-
+  if (req.method === "OPTIONS") return;
   res.setHeader(
     'Access-Control-Allow-Origin', 'https://tutoring-platform-becky.vercel.app'
   )
@@ -24,7 +23,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Content-Type, Authorization, Accept, Accept-Encoding,authorization'
   )
-
+  
   next()
 })
 
