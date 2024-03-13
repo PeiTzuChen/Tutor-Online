b6484e2f11d7c97ada6d9aa873a1549b6e7e504f
peitzuchen <prettysna@hotmail.com>
2024-03-13 20:19:55 +0800

    change config for zeabur

--- config/config.js
+++ config/config.js
@@ -14,20 +14,20 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'mysql'
   },
-  // production: { // for zeabur
-  //   username: process.env.MYSQL_USERNAME,
-  //   password: process.env.MYSQL_PASSWORD,
-  //   database: process.env.MYSQL_DATABASE,
-  //   host: process.env.MYSQL_HOST,
-  //   port: process.env.MYSQL_PORT,
-  //   dialect: 'mysql'
-  // }
-  production: { // for docker
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
+  production: { // for zeabur
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_ROOT_HOST,
+    host: process.env.MYSQL_HOST,
     port: process.env.MYSQL_PORT,
     dialect: 'mysql'
   }
+  // production: { // for docker
+  //   username: process.env.MYSQL_USER,
+  //   password: process.env.MYSQL_ROOT_PASSWORD,
+  //   database: process.env.MYSQL_DATABASE,
+  //   host: process.env.MYSQL_ROOT_HOST,
+  //   port: process.env.MYSQL_PORT,
+  //   dialect: 'mysql'
+  // }
 }
--- test.js
+++
@@ -1,6 +0,0 @@
-const a = 1
-const b = 9
-
-console.log(a + b)
-const c = 6
-module.exports = c
